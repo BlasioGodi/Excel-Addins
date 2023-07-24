2a5182442da11985f4618070d3d517eea957d599
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-07-24 23:59:59 +0300

    Files updated

--- CSVAutoUpdate/CSVAutoupdate/CSVAutoupdate/Program.cs
+++ CSVAutoUpdate/CSVAutoupdate/CSVAutoupdate/Program.cs
@@ -1,11 +1,18 @@
-﻿class Program
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
 {
+    // Create a lock object for file access synchronization
+    private static readonly object fileLock = new object();
+
     static void Main()
     {
         // File paths
-        string sourceFilePath = @"C:\Users\user\Desktop\MT5_TERMINALS\PATH_1\Original.csv";
-        string destinationFolder1 = @"C:\Users\user\Desktop\MT5_TERMINALS\PATH_2\";
-        string destinationFolder2 = @"C:\Users\user\Desktop\MT5_TERMINALS\PATH_3\";
+        string sourceFilePath = @"C:\Program Files\MT5-ACC1\MQL5\Files\MasterEASignal.csv";
+        string destinationFolder1 = @"C:\Program Files\MT5-ACC2\MQL5\Files\";
+        string destinationFolder2 = @"C:\Program Files\MT5-ACC3\MQL5\Files\";
 
         // Start a continuous update loop
         while (true)
@@ -13,25 +20,29 @@
             // Check if the source CSV file has been updated
             if (File.GetLastWriteTime(sourceFilePath) > GetLastUpdateTime())
             {
-                // Read data from the source CSV file
-                string[] lines = File.ReadAllLines(sourceFilePath);
-
-                // Process and transfer data to other CSV files
-                foreach (string line in lines)
+                // Use a lock to ensure exclusive access to the file during reading
+                lock (fileLock)
                 {
-                    // Split the CSV data assuming comma-separated values
-                    string[] data = line.Split(',');
+                    // Read data from the source CSV file
+                    string[] lines = File.ReadAllLines(sourceFilePath);
 
-                    // Process the data if needed
-                    string processedData = ProcessData(data);
+                    // Process and transfer data to other CSV files
+                    foreach (string line in lines)
+                    {
+                        // Split the CSV data assuming comma-separated values
+                        string[] data = line.Split(',');
 
-                    // Transfer data to the first destination CSV file
-                    string destinationFile1 = Path.Combine(destinationFolder1, "destination1.csv");
-                    TransferData(destinationFile1, processedData);
+                        // Process the data if needed
+                        string processedData = ProcessData(data);
 
-                    // Transfer data to the second destination CSV file
-                    string destinationFile2 = Path.Combine(destinationFolder2, "destination2.csv");
-                    TransferData(destinationFile2, processedData);
+                        // Transfer data to the first destination CSV file
+                        string destinationFile1 = Path.Combine(destinationFolder1, "SlaveEASignal1.csv");
+                        TransferData(destinationFile1, processedData);
+
+                        // Transfer data to the second destination CSV file
+                        string destinationFile2 = Path.Combine(destinationFolder2, "SlaveEASignal2.csv");
+                        TransferData(destinationFile2, processedData);
+                    }
                 }
 
                 // Update the last update time to avoid processing the same data multiple times
@@ -39,7 +50,7 @@
             }
 
             // Wait for a specified time before checking for updates again (e.g., 1 minute)
-            Thread.Sleep(60000);
+            Thread.Sleep(30000);
         }
     }
 
@@ -47,16 +58,17 @@
     static string ProcessData(string[] data)
     {
         // For simplicity, just concatenate the first and second columns
-        return data[0] + "," + data[1];
+        return data[0] + "," + data[1]+ "," + data[2] + "," + data[3];
     }
 
     // Write data to the destination CSV file
     static void TransferData(string destinationFile, string data)
     {
-        // Append data to the destination CSV file
-        using (StreamWriter writer = new StreamWriter(destinationFile, true))
+        // Use a lock to ensure exclusive access to the file during writing
+        lock (fileLock)
         {
-            writer.WriteLine(data);
+            // Clear the existing data in the destination file before writing new data
+            File.WriteAllText(destinationFile, data);
         }
     }
 
