c79a0293f58c0a5fa656d6cddb48c24154e7e03a
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-07-25 20:04:37 +0300

    Modified asynchronous file access

--- CSVAutoUpdate/CSVAutoupdate/CSVAutoupdate/Program.cs
+++ CSVAutoUpdate/CSVAutoupdate/CSVAutoupdate/Program.cs
@@ -1,74 +1,95 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
-
+using System.Threading.Tasks;
 class Program
 {
     // Create a lock object for file access synchronization
     private static readonly object fileLock = new object();
 
-    static void Main()
+    static async Task Main()
     {
         // File paths
         string sourceFilePath = @"C:\Program Files\MT5-ACC1\MQL5\Files\MasterEASignal.csv";
         string destinationFolder1 = @"C:\Program Files\MT5-ACC2\MQL5\Files\";
         string destinationFolder2 = @"C:\Program Files\MT5-ACC3\MQL5\Files\";
 
+        int maxRetries = 10; // Set the maximum number of retry attempts
+
         // Start a continuous update loop
         while (true)
         {
-            // Check if the source CSV file has been updated
-            if (File.GetLastWriteTime(sourceFilePath) > GetLastUpdateTime())
+            int retryCount = 0; // Initialize the retry counter
+
+            while (retryCount <= maxRetries)
             {
-                // Use a lock to ensure exclusive access to the file during reading
-                lock (fileLock)
+                try
                 {
-                    // Read data from the source CSV file
-                    string[] lines = File.ReadAllLines(sourceFilePath);
-
-                    // Process and transfer data to other CSV files
-                    foreach (string line in lines)
+                    // Check if the source CSV file has been updated
+                    if (File.GetLastWriteTime(sourceFilePath) > GetLastUpdateTime())
                     {
-                        // Split the CSV data assuming comma-separated values
-                        string[] data = line.Split(',');
+                        // Read data from the source CSV file asynchronously
+                        string[][] rows = await ReadCsvFileAsync(sourceFilePath);
 
-                        // Process the data if needed
-                        string processedData = ProcessData(data);
+                        // Process and transfer data to other CSV files
+                        foreach (string[] data in rows)
+                        {
+                            // Process the data if needed
+                            string processedData = ProcessData(data);
 
-                        // Transfer data to the first destination CSV file
-                        string destinationFile1 = Path.Combine(destinationFolder1, "SlaveEASignal1.csv");
-                        TransferData(destinationFile1, processedData);
+                            // Transfer data to the first destination CSV file asynchronously
+                            string destinationFile1 = Path.Combine(destinationFolder1, "SlaveEASignal1.csv");
+                            await TransferDataAsync(destinationFile1, processedData);
 
-                        // Transfer data to the second destination CSV file
-                        string destinationFile2 = Path.Combine(destinationFolder2, "SlaveEASignal2.csv");
-                        TransferData(destinationFile2, processedData);
+                            // Transfer data to the second destination CSV file asynchronously
+                            string destinationFile2 = Path.Combine(destinationFolder2, "SlaveEASignal2.csv");
+                            await TransferDataAsync(destinationFile2, processedData);
+                        }
+
+                        // Update the last update time to avoid processing the same data multiple times
+                        SetLastUpdateTime(DateTime.Now);
                     }
                 }
-
-                // Update the last update time to avoid processing the same data multiple times
-                SetLastUpdateTime(DateTime.Now);
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                // Wait for a specified time before checking for updates again (e.g., 1 minute)
+                await Task.Delay(1000);
             }
-
             // Wait for a specified time before checking for updates again (e.g., 1 minute)
-            Thread.Sleep(30000);
+            await Task.Delay(1000);
         }
     }
 
-    // Process data (you can customize this based on your needs)
-    static string ProcessData(string[] data)
+// Process data (you can customize this based on your needs)
+static string ProcessData(string[] data)
+{
+    // Ensure that the data array has at least six elements
+    if (data.Length < 6)
     {
-        // For simplicity, just concatenate the first and second columns
-        return data[0] + "," + data[1]+ "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5];
+        // Handle the case where the data is incomplete or incorrect
+        // For example, you can log an error or return an empty string
+        return string.Empty;
     }
 
-    // Write data to the destination CSV file
-    static void TransferData(string destinationFile, string data)
+    // Trim each element to remove any whitespaces
+    for (int i = 0; i < data.Length; i++)
     {
-        // Use a lock to ensure exclusive access to the file during writing
-        lock (fileLock)
+        data[i] = data[i].Trim();
+    }
+
+    // For simplicity, just concatenate the first and second columns
+    return data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5];
+}
+
+
+    // Write data to the destination CSV file asynchronously
+    static async Task TransferDataAsync(string destinationFile, string data)
+    {
+        using (StreamWriter writer = new StreamWriter(destinationFile, false))
         {
-            // Clear the existing data in the destination file before writing new data
-            File.WriteAllText(destinationFile, data);
+            // Write the data asynchronously to the file
+            await writer.WriteAsync(data);
         }
     }
 
@@ -85,4 +106,32 @@ class Program
         // Implement your logic to store the last update time
         // For simplicity, we won't store the time in this example
     }
+
+    static async Task<string[][]> ReadCsvFileAsync(string filePath)
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            // Initialize a list to store rows
+            List<string[]> rowsList = new List<string[]>();
+
+            // Read each line asynchronously
+            while (!reader.EndOfStream)
+            {
+                string line = await reader.ReadLineAsync();
+
+                // Skip empty lines
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    // Split the line into columns based on commas
+                    string[] columns = line.Split(',');
+
+                    rowsList.Add(columns);
+                }
+            }
+
+            return rowsList.ToArray();
+        }
+    }
+
+
 }
